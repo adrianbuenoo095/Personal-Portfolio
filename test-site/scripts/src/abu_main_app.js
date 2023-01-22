fae7e38dcccefe37fc1bb894ef6d55f68478db24
Adrian <adrianbueno095@gmail.com>
2023-01-22 15:21:08 +0100

    refactor code

--- test-site/scripts/src/abu_main_app.js
+++ test-site/scripts/src/abu_main_app.js
@@ -1,11 +1,8 @@
 import { hidesNavigationBar, openNavigationBar } from "./abu_sidebarMenu";
 /**
  * Author: Adrian Bueno
- *
- *
- *
- *
  */
+
 window.addEventListener("load", onLoad);
 function onLoad(evt) {
   console.log("page is fully loaded");

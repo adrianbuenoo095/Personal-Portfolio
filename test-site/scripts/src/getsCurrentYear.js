bff9080e18f990f1c257f0bceaa581a15db6202e
Adrian <adrianbueno095@gmail.com>
2023-01-23 17:34:01 +0100

    create a new script to get the current year

---
+++ test-site/scripts/src/getsCurrentYear.js
@@ -0,0 +1,6 @@
+export function getsCurrentYear() {
+  let dateElement = document.getElementById("date");
+  let date = new Date();
+  let fullYear = date.getFullYear();
+  return (dateElement.innerHTML = fullYear);
+}

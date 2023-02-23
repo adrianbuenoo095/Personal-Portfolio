415a8ed26c6fec75df3073dbd073e635b04cbd21
Adrian <adrianbueno095@gmail.com>
2023-02-23 13:44:36 +0100

    refactor code

--- test-site/scripts/src/getsCurrentYear.js
+++ test-site/scripts/src/getsCurrentYear.js
@@ -2,5 +2,5 @@ export function getsCurrentYear() {
   let dateElement = document.getElementById("date");
   let date = new Date();
   let fullYear = date.getFullYear();
-  return (dateElement.innerHTML = `${fullYear}`);
+  return dateElement.innerHTML = `${fullYear}`;
 }

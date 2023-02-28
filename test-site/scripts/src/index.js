d4dfa4a00f61d247de177767a724d5d52e30dfc9
Adrian <adrianbueno095@gmail.com>
2023-02-28 11:53:58 +0100

    install lodash locally, move files

--- test-site/scripts/src/index.js
+++ test-site/scripts/src/index.js
@@ -1,7 +1,10 @@
+
+import _ from 'lodash'; 
+
 function component(){
     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['hello', 'webpack']);
+    element.innerHTML = _.join(['hello', 'webpack'], '');
 
     return element;
 }

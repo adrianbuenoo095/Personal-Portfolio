cfd04e2b89592c4dbff9bb6d0d4ca1241438ee09
Adrian <adrianbueno095@gmail.com>
2023-03-05 14:11:11 +0100

    fix issue with webpack-cli

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-
-import _ from 'lodash';
+import _ from "lodash";
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement("div");
 
-    //Lodash, now imported by this script
-    element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  //Lodash, now imported by this script
+  element.innerHTML = _.join(["hello", "webpack"], " ");
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());

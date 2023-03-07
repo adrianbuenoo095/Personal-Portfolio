f6bdd456914a08d207748dcab1c430e76bac6146
Adrian <adrianbueno095@gmail.com>
2023-03-07 13:12:39 +0100

    add a new style.css, update index.js, reformat code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import _ from "lodash";
+import './style.css'
 import printMe from "./print.js";
+
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement("button");
   //Lodash, now imported by this script
   element.innerHTML = _.join(["hello", "webpack"], " ");
   btn.innerHTML = 'Click me and check the console';
+  element.classList.add('hello');
   btn.onclick = printMe;
-  
+
   element.appendChild(btn);
   return element;
 }

412e864377d20f2bdee38df736a3859c492dd6ac
Adrian <adrianbueno095@gmail.com>
2023-03-23 11:11:42 +0100

    update tsconfig.json and add new code to index.ts

--- adrian_bueno_portfolio/src/index.ts
+++ adrian_bueno_portfolio/src/index.ts
@@ -1,1 +1,10 @@
 import * as _ from 'lodash';
+
+function component() : Element{
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());

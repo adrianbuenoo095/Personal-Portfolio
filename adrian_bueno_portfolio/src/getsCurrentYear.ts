22142c734cd4f26fc134b1ce4f2ca0d1e6da0cb0
Adrian <adrianbueno095@gmail.com>
2023-03-26 23:49:09 +0200

    fix problems in scripts

--- adrian_bueno_portfolio/src/getsCurrentYear.ts
+++ adrian_bueno_portfolio/src/getsCurrentYear.ts
@@ -1,5 +1,5 @@
 export function getsCurrentYear() {
-  let dateElement = document.getElementById("date");
+  let dateElement = document.getElementById("date")!;
   let date = new Date();
   let fullYear = date.getFullYear();
   return (dateElement.innerHTML = `${fullYear}`);

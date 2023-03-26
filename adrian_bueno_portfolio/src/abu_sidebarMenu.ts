08086b31d2d9dd5169c9f0588107a01700a2552a
Adrian <adrianbueno095@gmail.com>
2023-03-26 23:54:03 +0200

    refactor code

--- adrian_bueno_portfolio/src/abu_sidebarMenu.ts
+++ adrian_bueno_portfolio/src/abu_sidebarMenu.ts
@@ -1,13 +1,13 @@
-const hamburgerIcon = document.querySelector(".hamburger")!;
-const navLinks = document.querySelector(".nav-links")!;
+const hamburgerIcon = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-hamburgerIcon.addEventListener("click", opensNavigationBar);
-hamburgerIcon.addEventListener("click", hidesNavigationBar);
+hamburgerIcon?.addEventListener("click", opensNavigationBar);
+hamburgerIcon?.addEventListener("click", hidesNavigationBar);
 
-function opensNavigationBar() : void{
+function opensNavigationBar(): void {
   navLinks?.classList.toggle("open");
 }
 
-function hidesNavigationBar() : void {
+function hidesNavigationBar(): void {
   navLinks?.classList.toggle("fade");
 }

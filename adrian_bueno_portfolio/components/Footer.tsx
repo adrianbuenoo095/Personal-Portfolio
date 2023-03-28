47d97a66c43d9c42d82faeba4238ff74f8655e74
Adrian <adrianbueno095@gmail.com>
2023-03-28 20:54:27 +0200

    add a new footer, create a new folder called components

---
+++ adrian_bueno_portfolio/components/Footer.tsx
@@ -0,0 +1,10 @@
+import  * as React from 'react';
+import { getsCurrentYear } from '@/src/getsCurrentYear';
+
+export default function Foooter {
+    return (
+    <footer id="footer" className="footer-section">
+      <p>&copy; <em id="date"> </em>&nbsp;The pocman</p>
+    </footer>
+    );
+}

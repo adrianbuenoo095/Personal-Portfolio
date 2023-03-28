787a99a54b78b1d232af1394ede1afb27512679b
Adrian <adrianbueno095@gmail.com>
2023-03-28 21:08:32 +0200

    refactor code

--- adrian_bueno_portfolio/components/Footer.tsx
+++ adrian_bueno_portfolio/components/Footer.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { getsCurrentYear } from "@/src/getsCurrentYear";
+import { getsCurrentYear } from "../src/getsCurrentYear";
 
 export default function Foooter() {
+  const date = getsCurrentYear();
   return (
     <footer id="footer" className="footer-section">
       <p>
-        &copy; <em id="date"> </em>&nbsp;The pocman
+        &copy; <em>{date}</em>&nbsp;The pocman
       </p>
     </footer>
   );

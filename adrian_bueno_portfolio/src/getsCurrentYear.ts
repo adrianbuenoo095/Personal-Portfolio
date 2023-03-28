29c76181e315351e55027efcd65dd5a6a5f73bcd
Adrian <adrianbueno095@gmail.com>
2023-03-28 20:18:22 +0200

    refactor code

--- adrian_bueno_portfolio/src/getsCurrentYear.ts
+++ adrian_bueno_portfolio/src/getsCurrentYear.ts
@@ -2,5 +2,6 @@ export function getsCurrentYear() : string {
   let dateElement = document.getElementById("date")!;
   let date = new Date();
   let fullYear = date.getFullYear();
-  return (dateElement.innerHTML = `${fullYear}`);
+  
+  return dateElement.innerHTML = `${fullYear}`;
 }

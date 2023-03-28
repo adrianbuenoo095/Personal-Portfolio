6d3cdeec4b2f43c2d2f9605e7a1c5fbfcb7a036d
Adrian <adrianbueno095@gmail.com>
2023-03-28 21:09:24 +0200

    fix typo in footer

--- adrian_bueno_portfolio/components/Footer.tsx
+++ adrian_bueno_portfolio/components/Footer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { getsCurrentYear } from "../src/getsCurrentYear";
 
-export default function Foooter() {
+export default function Footer() {
   const date = getsCurrentYear();
   return (
     <footer id="footer" className="footer-section">

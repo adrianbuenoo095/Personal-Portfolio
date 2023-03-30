8e1d77b096ab8b1cfd01c2c7c92c98fe3f143786
Adrian <adrianbueno095@gmail.com>
2023-03-30 14:50:45 +0200

    add a header and footer

--- adrian_bueno_portfolio/pages/index.tsx
+++ adrian_bueno_portfolio/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import { Imprima, Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +16,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
       <main className={styles.main}>
         <div className={styles.description}></div>
       </main>
+      <Footer/>
     </>
   );
 }

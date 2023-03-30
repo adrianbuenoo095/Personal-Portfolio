166a29a7e6529add32d08df25574605119ae2a78
Adrian <adrianbueno095@gmail.com>
2023-03-30 08:49:35 +0200

    refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,5 +1,13 @@
-import * as React from 'react';
+import * as React from "react";
 
 export class Header extends React.Component {
-    
+  render() {
+    return (
+      <div id="top" className="header">
+        <div className="intro-name">
+          <h1 className="">Hello, I am the Pocman</h1>
+        </div>
+      </div>
+    );
+  }
 }

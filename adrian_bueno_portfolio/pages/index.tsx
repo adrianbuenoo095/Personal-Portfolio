fc335138928f150456298d6b6196ae4bf8ab4007
Adrian <adrianbueno095@gmail.com>
2023-03-30 14:59:44 +0200

    refactor code

--- adrian_bueno_portfolio/pages/index.tsx
+++ adrian_bueno_portfolio/pages/index.tsx
@@ -1,12 +1,8 @@
 import Head from "next/head";
-import { Imprima, Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Navigation } from "@/components/Navigation";
-
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (

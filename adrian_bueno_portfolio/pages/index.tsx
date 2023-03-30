15a77a7a2aee46bd7f1f108d92276adbfd02e981
Adrian <adrianbueno095@gmail.com>
2023-03-30 14:58:34 +0200

    update index.tsx

--- adrian_bueno_portfolio/pages/index.tsx
+++ adrian_bueno_portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import { Imprima, Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { Navigation } from "@/components/Navigation";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navigation/>
       <Header/>
       <main className={styles.main}>
         <div className={styles.description}></div>

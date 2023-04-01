296b6e586125a71b83866cd299da4c9d78c82b12
Adrian <adrianbueno095@gmail.com>
2023-04-01 17:40:22 +0200

    add a new component

---
+++ adrian_bueno_portfolio/components/AboutMe.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export default class AboutMe extends React.Component {
+  render() {
+    return (
+      <div id="aboutMe" className="about">
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut la bore et dolore magna aliquyam
+          erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+          et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+    );
+  }
+}

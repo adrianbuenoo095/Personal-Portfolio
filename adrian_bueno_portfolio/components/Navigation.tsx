0fcdad9dbc226acedf7e75a930ef378e98954a79
Adrian <adrianbueno095@gmail.com>
2023-04-02 12:21:12 +0200

    add new style

--- adrian_bueno_portfolio/components/Navigation.tsx
+++ adrian_bueno_portfolio/components/Navigation.tsx
@@ -9,9 +9,11 @@ export class Navigation extends React.Component {
           <div className="line"></div>
           <div className="line"></div>
         </div>
-        <ul className="nav-links">
-          <li>
-            <a href="#">Home</a>
+        <ul className="flex items-center list-none w-3/6 h-full justify-around mx-auto">
+          <li className="text-white font-sans text-lg">
+            <a className="no-underline" href="#">
+              Home
+            </a>
           </li>
           <li>
             <a href="#aboutMe">About Me</a>

5c4e7587edd203f570666763e3a7f063d78f15aa
Adrian <adrianbueno095@gmail.com>
2023-04-03 22:34:30 +0200

    refactor code, fixed error

--- adrian_bueno_portfolio/pages/index.tsx
+++ adrian_bueno_portfolio/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import styles from "@/styles/index.module.css";
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
-import { Navigation } from "@/components/Navigation";
-import { Portfolio } from "@/components/Portfolio";
-import { AboutMe } from "@/components/AboutMe";
-import { Blog } from "@/components/Blog";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Navigation from "@/components/Navigation";
+import Portfolio from "@/components/Portfolio";
+import AboutMe from "@/components/AboutMe";
+import Blog from "@/components/Blog";
 
 export default function Home() {
   return (

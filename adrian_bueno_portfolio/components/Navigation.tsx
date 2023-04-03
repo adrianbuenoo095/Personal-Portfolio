ac4770caf9788200849358197b85a59ffe057697
Adrian <adrianbueno095@gmail.com>
2023-04-03 22:24:45 +0200

    refactor Navigation class into a function

--- adrian_bueno_portfolio/components/Navigation.tsx
+++ adrian_bueno_portfolio/components/Navigation.tsx
@@ -1,31 +1,31 @@
-import * as React from "react";
+import { ReactElement } from "react";
 
-export class Navigation extends React.Component {
-  render() {
-    return (
-      <nav>
-        <div className="hamburger">
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
-        <ul className="flex items-center list-none w-3/6 h-full justify-around mx-auto">
-          <li className="text-white font-sans text-lg">
-            <a className="no-underline" href="#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#aboutMe">About Me</a>
-          </li>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#blog">Blog</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+const Navigation = (): ReactElement => {
+  return (
+    <nav>
+      <div className="hamburger">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <ul className="flex items-center list-none w-3/6 h-full justify-around mx-auto">
+        <li className="text-white font-sans text-lg">
+          <a className="no-underline" href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#aboutMe">About Me</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#blog">Blog</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;

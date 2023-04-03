b08ee645068d3a6d889439b7c7406f7fd82d2972
Adrian <adrianbueno095@gmail.com>
2023-04-03 22:27:24 +0200

    refactor Porfolio class into a function

--- adrian_bueno_portfolio/components/Portfolio.tsx
+++ adrian_bueno_portfolio/components/Portfolio.tsx
@@ -1,22 +1,22 @@
-import * as React from "react";
+import { ReactElement } from "react";
 
-export class Portfolio extends React.Component {
-  render() {
-    return (
-      <div id="portfolio" className="portfolio">
-        <h1 className="testing">Adrian's Work</h1>
-        <ul className="skills-set">
-          <li>
-            <a href="">Web Designer</a>
-          </li>
-          <li>
-            <a href="">Illustrator</a>
-          </li>
-          <li>
-            <a href="">Full-Stack</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const Portfolio = (): ReactElement => {
+  return (
+    <div id="portfolio" className="portfolio">
+      <h1 className="testing">Adrian's Work</h1>
+      <ul className="skills-set">
+        <li>
+          <a href="">Web Designer</a>
+        </li>
+        <li>
+          <a href="">Illustrator</a>
+        </li>
+        <li>
+          <a href="">Full-Stack</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Portfolio;

338049d9f70bd654c511001155d1a2abcd9df082
Adrian <adrianbueno095@gmail.com>
2023-04-03 22:12:46 +0200

    refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
+import type { ReactElement } from "react";
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <div id="top" className="header">
-        <div className="intro-name">
-          <h1 className="">Hello, I am the Pocman</h1>
-        </div>
+const Header = (): ReactElement => {
+  return (
+    <div id="top" className="header">
+      <div className="intro-name">
+        <h1 className="">Hello, I am the Pocman</h1>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Header;

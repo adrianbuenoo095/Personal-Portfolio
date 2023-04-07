69cc6d3f0fdc23cfce8670bb2eb28eb35f4ba016
Adrian <adrianbueno095@gmail.com>
2023-04-07 15:29:43 +0200

    add a new image

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,10 +1,13 @@
 import type { ReactElement } from "react";
-
+import bg from "../images/bg.jpg";
 const Header = (): ReactElement => {
   return (
     <div id="top" className="header">
       <div className="intro-name">
         <h1 className="">Hello, I am the Pocman</h1>
+        <div className="backgroud-image">
+        <img src={String(bg)} width="100" height="50"/>
+      </div>
       </div>
     </div>
   );

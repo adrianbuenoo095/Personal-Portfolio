85d3d5ffb091f65911a22118de0f4669cde7b6d5
Adrian <adrianbueno095@gmail.com>
2023-04-07 15:36:43 +0200

    add a new image, refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,13 +1,14 @@
 import type { ReactElement } from "react";
 import bg from "../images/bg.jpg";
 const Header = (): ReactElement => {
+  console.log(bg);
   return (
     <div id="top" className="header">
       <div className="intro-name">
         <h1 className="">Hello, I am the Pocman</h1>
         <div className="backgroud-image">
-        <img src={String(bg)} width="100" height="50"/>
-      </div>
+          <img src={String(bg)} alt="background image" />
+        </div>
       </div>
     </div>
   );

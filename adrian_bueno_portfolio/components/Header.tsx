8e0c38e59075e3b841a92d850c31facc78a6a40b
Adrian <adrianbueno095@gmail.com>
2023-04-07 19:21:01 +0200

    refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,17 +1,16 @@
 import type { ReactElement } from "react";
-import bg from "../images/bg.jpg";
+import bg from  "assets/images/bg.jpg"
 const Header = (): ReactElement => {
-  console.log(bg);
   return (
     <div id="top" className="header">
       <div className="intro-name">
         <h1 className="">Hello, I am the Pocman</h1>
         <div className="backgroud-image">
-          <img src={String(bg)} alt="background image" />
+          
         </div>
       </div>
     </div>
   );
-};
+  };
 
 export default Header;

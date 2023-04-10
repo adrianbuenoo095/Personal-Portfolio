8f38fdd55b7e51f6027df7a5f08c33bc81d56356
Adrian <adrianbueno095@gmail.com>
2023-04-10 13:22:45 +0200

    install url-loader

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,13 +1,14 @@
 import type { ReactElement } from "react";
-import bg from  "assets/images/bg.jpg"
+import bg from  "../public/assets/images/bg.jpg"
 const Header = (): ReactElement => {
+  console.log(bg);
   return (
     <div id="top" className="header">
       <div className="intro-name">
         <h1 className="">Hello, I am the Pocman</h1>
         <div className="backgroud-image">
-          
         </div>
+          <img src={String(bg)} alt="background-image" />
       </div>
     </div>
   );

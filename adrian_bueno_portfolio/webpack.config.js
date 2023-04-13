f95d57c6116117929534515eb47fc167c29518db
Adrian <adrianbueno095@gmail.com>
2023-04-13 23:22:24 +0200

    update webpack

--- adrian_bueno_portfolio/webpack.config.js
+++ adrian_bueno_portfolio/webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader'
+            loader: 'file-loader',
+            options:{
+              esModule: false,
+            }
           }
         ]
       },

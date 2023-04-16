ac195e82dcafdbf3c9a4ce33bbcb392dc4803502
Adrian <adrianbueno095@gmail.com>
2023-04-16 23:21:15 +0200

    fix problem with image not displaying, refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -1,14 +1,12 @@
 import type { ReactElement } from "react";
-import backgroundImage from "./public/assets/images/backgroundImage.jpg" ;
-
 const Header = (): ReactElement => {
-  console.log(backgroundImage);
+  let backgroundImage = "assets/images/backgroundImage.jpg";
   return (
     <div id="top" className="header">
       <div className="intro-name">
         <h1 className="">Hello, I am the Pocman</h1>
         <div className="backgroud-image">
-          <img alt="background-image" />
+          <img src={backgroundImage} alt="background-image" />
         </div>
       </div>
     </div>

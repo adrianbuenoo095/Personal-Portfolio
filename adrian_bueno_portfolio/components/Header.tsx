7919ed30ca8dd491e60ed86020896695fa3bae06
Adrian <adrianbueno095@gmail.com>
2023-04-18 06:32:37 +0200

    add a new component

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -2,13 +2,18 @@ import type { ReactElement } from "react";
 
 const Header = (): ReactElement => {
   let backgroundImage = "assets/images/backgroundImage.jpg";
+
+  function Button() {
+    return (<button>Learn more</button>);
+  }
+
   return (
     <div className="header">
       <div className="intro-name">
         <div className="backgroud-image">
           <h1 className="">Hello, I am the Pocman</h1>
-          <input type="button" name="Click me" />
           <img src={backgroundImage} alt="background-image" />
+          <Button/>
         </div>
       </div>
     </div>

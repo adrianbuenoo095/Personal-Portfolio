34548cd60e8e793c5e22d2cbd73998cc2f64e23e
Adrian <adrianbueno095@gmail.com>
2023-04-21 00:39:30 +0200

    update tailwind.config

--- adrian_bueno_portfolio/tailwind.config.js
+++ adrian_bueno_portfolio/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

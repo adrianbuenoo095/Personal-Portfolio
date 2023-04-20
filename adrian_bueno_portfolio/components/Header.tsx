c9a71a480ace36e05e3bb73e65ec3da6228f31d0
Adrian <adrianbueno095@gmail.com>
2023-04-20 22:29:34 +0200

    add new tailwind style, update tailwing.config file

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -11,8 +11,10 @@ const Header = (): ReactElement => {
     <div className="header">
       <div className="intro-name">
         <div className="backgroud-image">
-          <h1 className="content-center">Hello, I am the Pocman</h1>
+        <div  className="flex h-screen justify-center items-center"> 
+          <h1 className="m-auto">Hello, I am the Pocman</h1>
           <Button />
+        </div>
           <img src={backgroundImage} alt="background-image" />
         </div>
       </div>

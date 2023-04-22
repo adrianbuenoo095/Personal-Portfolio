d9465223d05004643a85d06768dfb311e69b1910
Adrian <adrianbueno095@gmail.com>
2023-04-22 14:28:43 +0200

    update header and globals.css

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = (): ReactElement => {
       <div className="intro-name">
         <div className="relative h-32 bg-blue-400">
         <div  className="absolute inset-0 flex items-center justify-center"> 
-          <h1 className="text-3xl font-bold underlin">Hello, I am the Pocman</h1>
+          <h1 className="text-3xl font-bold underline">Hello, I am the Pocman</h1>
           <Button />
         </div>
           <img className="mx-auto" src={backgroundImage} alt="background-image" />

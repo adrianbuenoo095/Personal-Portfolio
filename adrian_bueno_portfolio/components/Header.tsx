9a8ecef7e1511017dd979e44d436130b1a67206c
Adrian <adrianbueno095@gmail.com>
2023-04-22 14:58:19 +0200

    update header

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = (): ReactElement => {
   let backgroundImage = "assets/images/backgroundImage.jpg";
 
   function Button() {
-    return <button className="">Learn more</button>;
+    return <button>Learn more</button>;
   }
 
   return (
@@ -12,10 +12,10 @@ const Header = (): ReactElement => {
       <div className="intro-name">
         <div className="relative h-32 bg-blue-400">
           <div className="absolute inset-0 flex items-center justify-center">
-            <h1 className="text-3xl font-bold underline">
+            <h1 className="text-3xl text-white font-bold underline ">
               Hello, I am the Pocman
             </h1>
-            <Button />
+          <Button />
           </div>
           <img
             className="mx-auto"

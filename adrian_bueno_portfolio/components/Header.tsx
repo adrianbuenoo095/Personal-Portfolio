f09e3ac70b11d2de87e2bb9f19e6de792472f591
Adrian <adrianbueno095@gmail.com>
2023-04-24 23:00:50 +0200

    refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -10,13 +10,17 @@ const Header = (): ReactElement => {
   return (
     <div className="header">
       <div className="flex h-screen">
-        <div className="relative w-40 h-40 overflow-hidden">
-          <h1>
+        <div className="relative w-40 h-40 overflow-hidden flex justify-center items-center">
+          <h1 className="text-white text-center z-10">
             Hello, I am the Pocman
           </h1>
-        <Button />
+          <Button />
         </div>
-        <img className="object-cover w-full h-full" src={backgroundImage} alt="background-image" />
+        <img
+          className="object-cover w-full h-full"
+          src={backgroundImage}
+          alt="background-image"
+        />
       </div>
     </div>
   );

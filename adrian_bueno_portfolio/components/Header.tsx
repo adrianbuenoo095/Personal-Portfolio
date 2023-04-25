723f41e5d751e53cc00e828060b955a89acb19fd
Adrian <adrianbueno095@gmail.com>
2023-04-25 20:51:30 +0200

    refactor code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -4,14 +4,14 @@ const Header = (): ReactElement => {
   let backgroundImage = "assets/images/backgroundImage.jpg";
 
   function Button() {
-    return <button>Learn more</button>;
+    return <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl text-white text-center z-10">Learn more</button>;
   }
 
   return (
     <div className="header">
       <div className="flex h-screen">
         <div className="relative w-full h-full flex flex-col justify-center items-center">
-          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center z-10">
+          <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl	 text-white text-center z-10">
             Hello, I am the Pocman
           </h1>
           <Button />

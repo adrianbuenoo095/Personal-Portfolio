ec05d2562cefd526f7d7c057669356a4d84ca1e5
Adrian <adrianbueno095@gmail.com>
2023-04-26 18:40:57 +0200

    reformat code

--- adrian_bueno_portfolio/components/Header.tsx
+++ adrian_bueno_portfolio/components/Header.tsx
@@ -15,13 +15,13 @@ const Header = (): ReactElement => {
 
   return (
     <div className="header">
-        <div className="flex flex-col justify-center items-center h-screen bg-cover">
-          <h1 className="text-4xl font-bold text-black">
-            Hello, I am the Pocman
-          </h1>
-          <Button />
-          <img src={backgroundImage} alt="background-image" />
-        </div> 
+      <div className="flex flex-col justify-center items-center h-screen bg-cover">
+        <h1 className="text-4xl font-bold text-black">
+          Hello, I am the Pocman
+        </h1>
+        <Button />
+        <img src={backgroundImage} alt="background-image" />
+      </div>
     </div>
   );
 };

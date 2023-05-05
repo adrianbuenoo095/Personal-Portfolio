cdff09b9ec257d62d10194a91527b07e0204f014
Adrian <adrianbueno095@gmail.com>
2023-05-05 23:38:18 +0200

    update aboutme site

--- adrian_bueno_portfolio/components/AboutMe.tsx
+++ adrian_bueno_portfolio/components/AboutMe.tsx
@@ -7,15 +7,15 @@ const AboutMe = (): ReactElement => {
         <h1 className="text-white font-bold text-8xl ">Who is the pocman?</h1>
       </div>
       <p className="text-3xl my-8 w-full">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut la bore et dolore magna aliquyam erat, sed
-        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-        ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+        The Pocman is an Instagram account that features artwork and comics
+        created by an artist known as POC or "Person of Color" man. POC man uses
+        their platform to showcase their unique perspective on current events,
+        pop culture, and everyday life through a combination of illustrations
+        and humorous captions. Their work often addresses issues of social
+        justice and diversity, making their art both entertaining and
+        thought-provoking. With a growing following on Instagram, the Pocman has
+        become a prominent voice in the online art community, inspiring and
+        engaging followers from all walks of life.
       </p>
     </div>
   );

d32c9077c88d9c4bec8382a9342cf91014006d0d
Adrian <adrianbueno095@gmail.com>
2023-05-06 14:37:31 +0200

    reformat code

--- adrian_bueno_portfolio/components/NavBar.tsx
+++ adrian_bueno_portfolio/components/NavBar.tsx
@@ -1,28 +1,28 @@
 import { ReactElement } from "react";
 /**
- * 
- * @todo look at this link  https://codewithmarish.com/post/how-to-create-responsive-navbar-in-next-js 
+ *
+ * @todo look at this link  https://codewithmarish.com/post/how-to-create-responsive-navbar-in-next-js
  */
 const Navigation = (): ReactElement => {
   return (
     <nav className="my-11">
       <div className="text-black font-sans text-lg ">
-      <ul className="flex items-center list-none w-3/6 h-full justify-around mx-auto">
-        <li>
-          <a className="no-underline" href="#">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#aboutMe">About Me</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#blog">Blog</a>
-        </li>
-      </ul>
+        <ul className="flex items-center list-none w-3/6 h-full justify-around mx-auto">
+          <li>
+            <a className="n" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#aboutMe">About Me</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );

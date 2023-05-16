9361cbc7b3d1a89f5f600c2663f484544dc22d6e
Adrian <adrianbueno095@gmail.com>
2023-05-16 18:42:32 +0200

    add a new icon

--- adrian_bueno_portfolio/components/Footer.tsx
+++ adrian_bueno_portfolio/components/Footer.tsx
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { ReactElement } from  "react";
+import {faStickyNote} from "@fortawesome/free-regular-svg-icons"
 
 const Footer = (): ReactElement => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer id="footer" className="text-center">
+      <FontAwesomeIcon icon={faStickyNote}/>
       <p>&copy; The pocman {currentYear}&nbsp;</p>
     </footer>
   );

28ffc61283d27bf28a769fc6c2755a76eb6a2bb5
Adrian <adrianbueno095@gmail.com>
2023-05-25 14:40:39 +0200

    refactor code

--- adrian_bueno_portfolio/components/NavBar.tsx
+++ adrian_bueno_portfolio/components/NavBar.tsx
@@ -1,31 +1,22 @@
 import Link from "next/link";
 import { ReactElement, useState } from "react";
 
-interface Props {
-  text: string;
-  href: string;
-  active?: boolean;
-}
-
 const MENU_LIST = [
-  { text: "Home", href: "/" },
-  { text: "About Me", href: "/aboutMe" },
-  { text: "Portfolio", href: "/portfolio" },
-  { text: "Blog", href: "/blog" },
+  { name: "Home", path: "/" },
+  { name: "About Me", path: "/aboutMe" },
+  { name: "Portfolio", path: "/portfolio" },
+  { name: "Blog", path: "/blog" },
 ];
 
-const NavBar = ({ text, href, active }: Props): ReactElement => {
-  const [navActive, setNavActive] = useState();
-  const [activeId, setActiveId] = useState();
-
+const NavBar = (): ReactElement => {
   return (
     <nav className="my-11">
       <div className="text-black font-sans text-lg ">
-        {MENU_LIST.map((href, idx) => {
+        {MENU_LIST.map((link, idx) => {
           return (
             <ul className="flex items-center list-none w-3/6 h-full justify-around mx-auto ">
-              <Link className="hover:text-teal-400" href={href.href}>
-                <li key={idx}> {href.href}</li>
+              <Link className="hover:text-teal-400" href={link.path}>
+                <li key={idx}> {link.name}</li>
               </Link>
             </ul>
           );

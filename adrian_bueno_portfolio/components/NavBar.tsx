c3cf6d683b13af1c55932440abf548557d8482ff
Adrian <adrianbueno095@gmail.com>
2023-05-27 22:34:56 +0200

    fix key problem

--- adrian_bueno_portfolio/components/NavBar.tsx
+++ adrian_bueno_portfolio/components/NavBar.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import { ReactElement } from "react";
 
+interface Props {
+  name: string;
+  path: string;
+  id: number;
+}
 const MENU_LIST = [
   { name: "Home", path: "/" },
   { name: "About Me", path: "/aboutMe" },
@@ -8,18 +13,19 @@ const MENU_LIST = [
   { name: "Blog", path: "/blog" },
 ];
 
-const NavBar = (): ReactElement => {
+const NavBar = ({ name, path, id }: Props): ReactElement => {
   return (
     <nav className="my-11">
       <div className="text-black font-sans text-lg ">
         {MENU_LIST.map((link, idx) => {
           return (
-            <div className="flex items-center list-none w-full h-full justify-around mx-auto">
-              <div key={idx}>
-                <Link className=" hover:text-teal-400" href={link.path}>
-                  {link.name}
-                </Link>
-              </div>
+            <div
+              key={idx}
+              className="flex items-center list-none w-full h-full justify-around mx-auto"
+            >
+              <Link className=" hover:text-teal-400" href={link.path}>
+                {link.name}
+              </Link>
             </div>
           );
         })}

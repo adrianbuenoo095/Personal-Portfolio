ba30d0e5d99b317ac1eddb033616890f9b17c51d
Adrian <adrianbueno095@gmail.com>
2023-06-05 23:47:51 +0200

    refactor code

--- adrian_bueno_portfolio/components/NavBar.tsx
+++ adrian_bueno_portfolio/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ReactElement } from "react";
 
 const MENU_LIST = [
@@ -12,21 +12,49 @@ const MENU_LIST = [
 const NavBar = (): ReactElement => {
   const [openMenu, setOpenMenu] = useState(false);
   const [activeIdx, setActiveIdx] = useState(-1);
+  const handleClick = useCallback(() => {
+    setOpenMenu((prevState) => {
+      return !prevState;
+    });
+  }, []);
 
   return (
-    <nav className="my-11">
-      <div className="text-black font-sans text-lg ">
-        {MENU_LIST.map((link, idx) => {
-          return (
-            <div key={idx} className="p-3 inline-flex">
-              <Link className="hover:text-teal-400" href={link.path}>
-                {link.name}
-              </Link>
-            </div>
-          );
-        })}
+    <>
+      <nav className="my-11 hidden md:block">
+        <div className="text-black font-sans text-lg ">
+          {MENU_LIST.map((link) => {
+            return (
+              <div key={link.name} className="p-3 inline-flex">
+                <Link className="hover:text-teal-400" href={link.path}>
+                  {link.name}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </nav>
+      <div
+        className="md:hidden fixed top-0 right-0 bg-gray-400 text-white"
+        onClick={handleClick}
+      >
+        Burger Menu
       </div>
-    </nav>
+      {openMenu && (
+        <div className="md:hidden fixed top-5 right-0 bg-gray-400 text-white p-4">
+          <div className="flex flex-col gap-5">
+          {MENU_LIST.map((link) => {
+            return (
+              <div key={link.name} className="p-3 inline-flex">
+                <Link className="hover:text-teal-400" href={link.path}>
+                  {link.name}
+                </Link>
+              </div>
+            );
+          })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ adrian_bueno_portfolio/pages/posts/[postId].tsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+
+export default function FirstPost() {
+  const router = useRouter();
+  console.log(router.query.postId)
+  return (
+    <div className=" h-screen w-screen bg-red-500">
+      <h1>First Post</h1>
+      <button
+        className="bg-lime-400"
+        type="button"
+        onClick={() => router.push("/posts/first")}
+      >
+        back
+      </button>
+    </div>
+  );
+}

0b133bd855f38756b91ab2d68e3d841ca3421794
Adrian <adrianbueno095@gmail.com>
2023-06-15 23:59:26 +0200

    reformat code

--- adrian_bueno_portfolio/pages/posts/[postId].tsx
+++ adrian_bueno_portfolio/pages/posts/[postId].tsx
@@ -2,7 +2,9 @@ import { useRouter } from "next/router";
 
 export default function FirstPost() {
   const router = useRouter();
-  console.log(router.query.postId)
+
+  console.log(router.query.postId);
+  
   return (
     <div className=" h-screen w-screen bg-red-500">
       <h1>First Post</h1>

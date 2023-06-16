bf69df8fc0279ea920ed7be9218890a8fc22e00b
Adrian <adrianbueno095@gmail.com>
2023-06-16 14:11:17 +0200

    add a fragment

--- adrian_bueno_portfolio/pages/posts/[postId].tsx
+++ adrian_bueno_portfolio/pages/posts/[postId].tsx
@@ -4,17 +4,19 @@ export default function FirstPost() {
   const router = useRouter();
 
   console.log(router.query.postId);
-  
+
   return (
-    <div className=" h-screen w-screen bg-red-500">
-      <h1>First Post</h1>
-      <button
-        className="bg-lime-400"
-        type="button"
-        onClick={() => router.back()}
-      >
-        back
-      </button>
-    </div>
+    <>
+      <div className=" h-screen w-screen bg-red-500">
+        <h1>First Post</h1>
+        <button
+          className="bg-lime-400"
+          type="button"
+          onClick={() => router.back()}
+        >
+          back
+        </button>
+      </div>
+    </>
   );
 }

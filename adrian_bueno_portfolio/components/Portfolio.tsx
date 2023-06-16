85a8f809d9e494e49b285ff2ede5e899f5674e23
Adrian <adrianbueno095@gmail.com>
2023-06-16 13:38:53 +0200

    refactor code

--- adrian_bueno_portfolio/components/Portfolio.tsx
+++ adrian_bueno_portfolio/components/Portfolio.tsx
@@ -2,20 +2,22 @@ import { ReactElement } from "react";
 
 const Portfolio = (): ReactElement => {
   return (
-    <div className="bg-yellow-500 w-screen h-screen text-center">
-      <h1>Adrian&apos;s Work</h1>
-      <div className="skills-set">
-        <div>
-          <h1>Web Designer</h1>
-        </div>
-        <div>
-          <h1>Illustrator</h1>
-        </div>
-        <div>
-          <h1>Full-Stack</h1>
+    <>
+      <div className="bg-yellow-500 w-screen h-screen text-center">
+        <h1>Adrian&apos;s Work</h1>
+        <div className="skills-set">
+          <div>
+            <h1>Web Designer</h1>
+          </div>
+          <div>
+            <h1>Illustrator</h1>
+          </div>
+          <div>
+            <h1>Full-Stack</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

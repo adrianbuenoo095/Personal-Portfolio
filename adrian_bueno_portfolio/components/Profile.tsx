03edd30e5379bbd2b42f66b28c9a7f551c059a87
Adrian <adrianbueno095@gmail.com>
2023-06-21 00:03:49 +0200

    refactor code

--- adrian_bueno_portfolio/components/Profile.tsx
+++ adrian_bueno_portfolio/components/Profile.tsx
@@ -5,11 +5,13 @@ type Props = {
   size: number;
 };
 
+let pocmanface = "/images/pocman_thick_version.png";
+
 const Avatar = ({ person, size }: Props) => {
   return (
     <Image
       className="avatar"
-      src="/images/pocman_thick_version.png"
+      src={pocmanface}
       alt={person.name}
       width={size}
       height={size}

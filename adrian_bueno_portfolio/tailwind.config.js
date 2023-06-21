7cca87dc56da9be28fa02d552698f6c310638efa
Adrian <adrianbueno095@gmail.com>
2023-06-21 15:54:55 +0200

    update the tailwind.config.js

--- adrian_bueno_portfolio/tailwind.config.js
+++ adrian_bueno_portfolio/tailwind.config.js
@@ -14,7 +14,26 @@ module.exports = {
       'md': '960px',
       'lg': '1440px'
     },
-    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem'
+      }, 
+      borderRadius :{
+        '4xl': '3rem'
+      }
+    },
   },
   plugins: [],
 };

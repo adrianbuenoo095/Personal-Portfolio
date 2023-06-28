fa29a9f19523bf0403ad1617811e8b4caefee23e
Adrian <adrianbueno095@gmail.com>
2023-06-28 16:56:20 +0200

    testing

--- adrian_bueno_portfolio/components/FancyBorder.tsx
+++ adrian_bueno_portfolio/components/FancyBorder.tsx
@@ -1,21 +1,31 @@
-import { ReactElement } from "react"
+import { ReactElement, useReducer } from "react";
 
 interface Button {
-    onClick: ()=>{};
-    children: Array<string>
+  onClick: () => {};
+  children: Array<string>;
+  icon: Element;
 }
 interface Props {
-    children : Array<string>;
-    color: string;
+  children: Array<string>;
+  color: string;
 }
 
-const Button =  ({onClick, children}: Button) => <button onClick={onClick}>{children}</button>;
+const reducer = (state: any, action: {]}) => {
 
-const FancyBorder = ({children, color}: Props): ReactElement => {
-return (
-    <div className={`'FancyBorder FanyBorder-'${color}` }>
-        {children}
-    </div>
-)
+};
+
+const MyComponent = (): ReactElement => {
+const [state, dispatch] = useReducer(reducer, {age:43});
+
+
+
 } 
+
+const Button = ({ onClick, children }: Button) => (
+  <button onClick={onClick}>{children}</button>
+);
+
+const FancyBorder = ({ children, color }: Props): ReactElement => {
+  return <div className={`'FancyBorder FanyBorder-'${color}`}>{children}</div>;
+};
 export default FancyBorder;

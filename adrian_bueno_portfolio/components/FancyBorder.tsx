6344097ba468570249565b80002b0e1eb5769065
Adrian <adrianbueno095@gmail.com>
2023-06-28 12:33:53 +0200

    new button

--- adrian_bueno_portfolio/components/FancyBorder.tsx
+++ adrian_bueno_portfolio/components/FancyBorder.tsx
@@ -1,9 +1,16 @@
 import { ReactElement } from "react"
 
+interface Button {
+    onClick: ()=>{};
+    children: Array<string>
+}
 interface Props {
     children : Array<string>;
     color: string;
 }
+
+const Button =  ({onClick, children}: Button) => <button onClick={onClick}>{children}</button>;
+
 const FancyBorder = ({children, color}: Props): ReactElement => {
 return (
     <div className={`'FancyBorder FanyBorder-'${color}` }>
@@ -11,3 +18,4 @@ return (
     </div>
 )
 } 
+export default FancyBorder;

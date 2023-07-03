0bc95999af462b8bbac0caf93054765db4d3f459
Adrian <adrianbueno095@gmail.com>
2023-07-03 18:24:33 +0200

    learning useMemo

--- adrian_bueno_portfolio/components/FancyBorder.tsx
+++ adrian_bueno_portfolio/components/FancyBorder.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useReducer } from "react";
+import { ReactElement, useMemo, useReducer } from "react";
 
 interface Action {
   type: string;
@@ -17,12 +17,18 @@ const reducer = (state: State, action: Action) => {
   throw Error("Unknown action.");
 };
 
+function greeting() {
+  return 'Hello World';
+}
 const Counter = (): ReactElement | null => {
   const [state, dispatch] = useReducer(reducer, { age: 34 });
+  const testingMemo = useMemo(() => greeting(), [state.age]);
+  console.log(` helloo world${testingMemo}`);
 
   return (
     <>
-      <button className="bg-blue"
+      <button
+        className="bg-blue"
         onClick={() => {
           dispatch({ type: "incremented_age" });
         }}

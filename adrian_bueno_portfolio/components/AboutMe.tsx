b75e819a58168c661a1ab9210a8ce5c17211e4e8
Adrian <adrianbueno095@gmail.com>
2023-07-06 22:49:19 +0200

    update text

--- adrian_bueno_portfolio/components/AboutMe.tsx
+++ adrian_bueno_portfolio/components/AboutMe.tsx
@@ -11,12 +11,12 @@ const AboutMe = (): ReactElement => {
             <Profile /> 
           </div>
           <p className="tracking-wide text-lg">
-            The Pocman is a great instagram account that features artwork and comics
+            The Pocman is a great instagram account that feature artwork and comics
             created by an artist known as POC or &quot;Person of Color&quot;
             man. POC man uses their platform to showcase their unique
             perspective on current events, pop culture, and everyday life
             through a combination of illustrations and humorous captions. Their
-            work often addresses issues of social justice and diversity, making
+            work often addresses issues of social justice and diversity, making®
             their art both entertaining and thought-provoking. With a growing
             following on Instagram, the Pocman has become a prominent voice in
             the online art community, inspiring and engaging followers from all

fb80cc06cc94f232911951f6c97f4ac3b4f572c5
Adrian <adrianbueno095@gmail.com>
2023-07-09 15:13:05 +0200

    refactor code

--- adrian_bueno_portfolio/components/AboutMe.tsx
+++ adrian_bueno_portfolio/components/AboutMe.tsx
@@ -4,13 +4,13 @@ import Profile from "./Profile";
 const AboutMe = (): ReactElement => {
   return (
     <>
-      <div id="AboutMe" className="h-screen bg-yellow">
+      <div id="AboutMe" className="h-screen bg-yellow  ">
         <div className="py-5">
           <div className="text-orange-600 font-bold text-8xl text-center uppercase">
             Who is the Pocman?
             <Profile /> 
           </div>
-          <p className="tracking-wide text-lg">
+          <p className="tracking-wide text-lg ">
             The Pocman is a great instagram account that features artwork and comics
             created by an artist known as POC or &quot;Person of Color&quot;
             man. POC man uses their platform to showcase their unique
@@ -21,6 +21,7 @@ const AboutMe = (): ReactElement => {
             following on Instagram, the Pocman has become a prominent voice in
             the online art community, inspiring and engaging followers from all
             walks of life.
+            
           </p>
         </div>
       </div>

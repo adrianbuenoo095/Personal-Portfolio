09bb039ccbab55e00a8cf3b0505e73d7f1cc20dc
Adrian <adrianbueno095@gmail.com>
2023-07-26 11:55:27 +0200

    refactor code

--- adrian_bueno_portfolio/components/common/Button.tsx
+++ adrian_bueno_portfolio/components/common/Button.tsx
@@ -1,10 +1,17 @@
 import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+
+const clickHandle = useCallback(()=>{
+  const router = useRouter();
+    
+  router.push("/posts/first-post")
+},[]);
 
 export let Button = () => {
-  const router = useRouter();
   return (
     <button
-      onClick={() => router.push("/posts/first-post")}
+      onClick={clickHandle}
       className="mt-6 px-4 py-2 bg-blue hover:bg-orange text-white font-bold rounded-lg "
     >
       Learn more

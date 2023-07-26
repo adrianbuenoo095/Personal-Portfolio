4941f4d898d363c0a4719c4b3a781fd73d4ffb2b
Adrian <adrianbueno095@gmail.com>
2023-07-26 12:29:34 +0200

    refacor button

--- adrian_bueno_portfolio/components/common/Button.tsx
+++ adrian_bueno_portfolio/components/common/Button.tsx
@@ -5,8 +5,10 @@ export let Button = (): ReactElement => {
   const router = useRouter();
   return (
     <button
-      onClick={()=>{router.push("/posts/first-post");}}
       className="mt-6 px-4 py-2 bg-blue hover:bg-orange text-white font-bold rounded-lg "
+      onClick={() => {
+        router.push("/posts/first-post");
+      }}
     >
       Learn more
     </button>

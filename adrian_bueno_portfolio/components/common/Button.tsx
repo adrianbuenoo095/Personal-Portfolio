15881e18e5bfe210f1d32893cc2f83bfbbf2cf84
Adrian <adrianbueno095@gmail.com>
2023-08-01 23:37:49 +0200

    refactor code

--- adrian_bueno_portfolio/components/common/Button.tsx
+++ adrian_bueno_portfolio/components/common/Button.tsx
@@ -3,12 +3,16 @@ import { ReactElement } from "react";
 
 export let Button = (): ReactElement => {
   const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    router.push("/posts/first-post");
+  };
+
   return (
     <button
       className="mt-6 px-4 py-2 bg-blue hover:bg-orange text-white font-bold rounded-lg "
-      onClick={() => {
-        router.push("/posts/first-post");
-      }}
+      onClick={handleClick}
     >
       Learn more
     </button>

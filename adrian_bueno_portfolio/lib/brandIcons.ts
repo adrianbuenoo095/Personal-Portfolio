4e15845ffea9b0d584f15343a6db0db0827b92e7
Adrian <adrianbueno095@gmail.com>
2023-08-04 23:32:15 +0200

    reformat codr

--- adrian_bueno_portfolio/lib/brandIcons.ts
+++ adrian_bueno_portfolio/lib/brandIcons.ts
@@ -1,12 +1,12 @@
-import type BrandIcon from "./brandIcon"; 
+import type BrandIcon from "./brandIcon";
 import {
-    faInstagram,
-    faLinkedin,
-    faBehance,
-  } from "@fortawesome/free-brands-svg-icons";
+  faInstagram,
+  faLinkedin,
+  faBehance,
+} from "@fortawesome/free-brands-svg-icons";
 
-  export const brandIcons = new Array<BrandIcon>(
-   {key: 'instagram', label: 'Instagram', icon:faInstagram},
-   {key: 'linkedin', label: 'Linkedin', icon:faLinkedin},
-   {key: 'behance', label: 'Behance', icon:faBehance},
-  )
+export const brandIcons = new Array<BrandIcon>(
+  { key: "instagram", label: "Instagram", icon: faInstagram },
+  { key: "linkedin", label: "Linkedin", icon: faLinkedin },
+  { key: "behance", label: "Behance", icon: faBehance }
+);

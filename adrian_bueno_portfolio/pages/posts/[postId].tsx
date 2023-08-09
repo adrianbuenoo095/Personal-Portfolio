5c8156a72e4ac824388e3e4059d84efd06497aa0
Adrian <adrianbueno095@gmail.com>
2023-08-09 11:42:05 +0200

    refactor code

--- adrian_bueno_portfolio/pages/posts/[postId].tsx
+++ adrian_bueno_portfolio/pages/posts/[postId].tsx
@@ -1,21 +1,20 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function FirstPost() {
   const router = useRouter();
+  const id = router.query.id;
+  console.log(id);
 
-  console.log(router.query.postId);
+  if (id === null) {
+    return <h1>Loading</h1>;
+  }
 
   return (
     <>
       <div className=" h-screen w-screen bg-pink">
-        <h1>First Post</h1>
-        <button
-          className="bg-orange"
-          type="button"
-          onClick={() => router.back()}
-        >
-          Take me back
-        </button>
+        <h1>First Post {id}</h1>
+        <Link href="/">Take me back</Link>
       </div>
     </>
   );

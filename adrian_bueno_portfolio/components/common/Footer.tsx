294cc89a8b9477522820aafa69dc3aba9671b9b8
Adrian <adrianbueno095@gmail.com>
2023-08-09 09:05:36 +0200

    add a target attribute

--- adrian_bueno_portfolio/components/common/Footer.tsx
+++ adrian_bueno_portfolio/components/common/Footer.tsx
@@ -8,19 +8,31 @@ import {
 import Link from "next/link";
 
 const instagramLink = (
-  <Link href="https://www.instagram.com/thepocman/" className="p-3 text-lg">
+  <Link
+    href="https://www.instagram.com/thepocman/"
+    className="p-3 text-lg"
+    target="_blank"
+  >
     <FontAwesomeIcon icon={faInstagram} />
   </Link>
 );
 
 const linkedinLink = (
-  <Link href="https://www.linkedin.com/in/adrian-bueno-08189b108/" className="p-3 text-lg">
+  <Link
+    href="https://www.linkedin.com/in/adrian-bueno-08189b108/"
+    className="p-3 text-lg"
+    target="_blank"
+  >
     <FontAwesomeIcon icon={faLinkedin} />
   </Link>
 );
 
 const behanceLink = (
-  <Link href="https://www.behance.net/thepocman" className="p-3 text-lg">
+  <Link
+    href="https://www.behance.net/thepocman"
+    className="p-3 text-lg"
+    target="_blank"
+  >
     <FontAwesomeIcon icon={faBehance} />
   </Link>
 );
@@ -32,9 +44,11 @@ const Footer = (): ReactElement => {
     <>
       <footer id="footer" className="text-center">
         <div className="flex gap-3 justify-center">
-        <span>
-          {instagramLink}{linkedinLink}{behanceLink}
-        </span>
+          <span>
+            {instagramLink}
+            {linkedinLink}
+            {behanceLink}
+          </span>
         </div>
         <p>&copy; The Pocman {currentYear}&nbsp;</p>
       </footer>

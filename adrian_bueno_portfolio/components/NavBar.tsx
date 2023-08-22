a7c2f6eb8777f5454b9f7f8787fe5e076f4021df
Adrian <adrianbueno095@gmail.com>
2023-08-22 11:19:17 +0200

    remove code

--- adrian_bueno_portfolio/components/NavBar.tsx
+++ adrian_bueno_portfolio/components/NavBar.tsx
@@ -81,57 +81,6 @@ const NavBar = (): ReactElement => {
           </div>
         </div>
       )}
-      {/* <nav className="my-11 hidden md:block ">
-        {menuList.map((link) => {
-          return (
-            <div
-              key={link.name}
-              className="p-3 inline-flex text-black font-sans text-lg "
-            >
-              <Link
-                className="hover:text-pink"Rrl
-                href={link.path}
-                onClick={(e) => {
-                  e.preventDefault();
-                  scrollIntoTheView(link.id);
-                }}
-              >
-                {link.name}
-              </Link>
-            </div>
-          );
-        })}
-      </nav>
-      <div
-        className="md:hidden fixed top-3 right-4  text-white "
-        onClick={handleClick}
-      >
-        <Burger />
-      </div>
-      {openMenu && (
-        <div className="md:hidden fixed top-5 right-0 bg-gray-dark text-white p-4">
-          <div className="flex flex-col gap-5">
-            {menuList.map((link) => {
-              return (
-                <div key={link.name} className="p-3 inline-flex">
-                  <Link
-                    className="hover:text-pink"
-                    href={link.path}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      if (link.id) {
-                        scrollIntoTheView(link.id);
-                      }
-                    }}
-                  >
-                    {link.name}
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      )} */}
     </>
   );
 };

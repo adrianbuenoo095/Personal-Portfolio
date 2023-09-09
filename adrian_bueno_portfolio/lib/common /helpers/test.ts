9eeecd3323d0766eb85f8a358864eeb2a8b862c5
Adrian <adrianbueno095@gmail.com>
2023-09-09 12:52:41 +0200

    add a new test file

---
+++ adrian_bueno_portfolio/lib/common /helpers/test.ts
@@ -0,0 +1,33 @@
+import ListItem from '@material-ui/core/ListItem';
+import { useEffect, useState } from 'react';
+
+interface User {
+    name: string;
+    email: string;
+    userId:number
+}
+
+function User({userId}: User) {
+
+    const [user, setUser] = useState({name: '', email:''});
+    const fetchUser = async () => {
+        const res = await fetch(
+            `https://jsonplaceholder.typicode.com/users/${userId}`
+        );
+        const newUser = await res.json();
+        setUser(newUser);
+    };
+
+    useEffect(() => {
+        fetchUser();
+    })
+
+    return(
+        <ListItem dense divider>
+        <ListItem primary={user.name} secondary={user.email}>
+        </ListItem>
+        )
+    
+}
+
+export default User:

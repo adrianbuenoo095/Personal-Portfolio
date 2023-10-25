b4bc0b7c30aee505f8d513b04e87e334a99440a2
Adrian <adrianbueno095@gmail.com>
2023-10-25 14:14:10 +0200

    fix icon's size

--- adrian_bueno_portfolio/components/common/Footer.tsx
+++ adrian_bueno_portfolio/components/common/Footer.tsx
@@ -5,32 +5,23 @@ import Link from "next/link";
 import type { ReactElement } from "react";
 
 const instagramLink = (
-    <Link
-        href="https://www.instagram.com/thepocman/"
-        className="p-3"
-        target="_blank"
-    >
-        <FontAwesomeIcon icon={faInstagram} />
+    <Link href="https://www.instagram.com/thepocman/" target="_blank">
+        <FontAwesomeIcon className="w-[25px]" icon={faInstagram} />
     </Link>
 );
 
 const linkedinLink = (
     <Link
         href="https://www.linkedin.com/in/adrian-bueno-08189b108/"
-        className="p-3"
         target="_blank"
     >
-        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon className="w-[25px]" icon={faLinkedin} />
     </Link>
 );
 
 const behanceLink = (
-    <Link
-        href="https://www.behance.net/thepocman"
-        className="p-3 "
-        target="_blank"
-    >
-        <FontAwesomeIcon icon={faBehance} />
+    <Link href="https://www.behance.net/thepocman" target="_blank">
+        <FontAwesomeIcon className="w-[25px]" icon={faBehance} />
     </Link>
 );
 
@@ -38,9 +29,12 @@ const Footer = (): ReactElement => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div id="footer" className="text-center ">
+        <div
+            id="footer"
+            className="text-sm text-black text-center p-4 bg-black space-y-1"
+        >
             <div>&copy; The Pocman {currentYear}&nbsp;</div>
-            <div className="flex gap-3 justify-center py-1 text-base">
+            <div className="flex gap-3 justify-center py-1">
                 <span>{instagramLink}</span>
                 <span>{linkedinLink}</span>
                 <span>{behanceLink}</span>

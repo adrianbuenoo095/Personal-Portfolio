8433dd00baea00bc8946f2cd2a4db1fd462577bb
Adrian <adrianbueno095@gmail.com>
2024-01-18 18:14:57 +0100

    add styling to contact form

--- adrian_bueno_portfolio/components/ContactForm.tsx
+++ adrian_bueno_portfolio/components/ContactForm.tsx
@@ -12,41 +12,56 @@ const ContactMe = (): ReactElement => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-5">
-                <label htmlFor="name" className="">
-                    Full Name
-                </label>
-                <input
-                    type="text"
-                    placeholder="Full Name"
-                    className=""
-                    {...register("name", { required: true })}
-                />
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <label
+                        htmlFor="name"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Full Name
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Full Name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        {...register("name", { required: true })}
+                    />
+                </div>
+                <div className="mb-6">
+                    <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Email Address
+                    </label>
+                    <input
+                        type="email"
+                        placeholder="example@domain.com"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        {...register("email", { required: true })}
+                    />
+                </div>
             </div>
-            <div className="mb-5">
-                <label htmlFor="email" className="">
-                    Email Address
-                </label>
-                <input
-                    type="email"
-                    placeholder="example@domain.com"
-                    className=""
-                    {...register("email", { required: true })}
-                />
+            <div className="mb-6">
+                <div>
+                    <label
+                        htmlFor="message"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                        Message
+                    </label>
+                    <textarea
+                        rows={4}
+                        placeholder="Type your message"
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        {...register("message", { required: true })}
+                    ></textarea>
+                </div>
             </div>
-            <div className="mb-5">
-                <label htmlFor="message" className="">
-                    Message
-                </label>
-                <textarea
-                    rows={4}
-                    placeholder="Type your message"
-                    className=""
-                    {...register("message", { required: true })}
-                ></textarea>
-            </div>
-            <div>
-                <button className="">Submit</button>
+            <div className="p-2 w-full">
+                <button className="text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Submit
+                </button>
             </div>
         </form>
     );

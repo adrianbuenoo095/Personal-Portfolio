dfd131e7df897139a2d1ab13a1fe1ab6e689f5e2
adrianbuenoo095 <adrianbueno095@gmail.com>
2024-12-30 12:19:36 +0100

    change name of the form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,11 +1,11 @@
 import type { ReactElement } from "react";
-import ContactMe from "./ContactForm";
+import ContactForm from "./ContactForm";
 
 const Contact = (): ReactElement => {
     return (
         <div id="Contact" className="bg-pink h-screen text-red">
             <div className="flex justify-center items-center">
-                <ContactMe />
+                <ContactForm />
             </div>
         </div>
     );

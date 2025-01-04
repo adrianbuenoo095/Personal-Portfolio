28a1e08ac9363623ec190e630156dae010a8dad1
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-01-05 00:08:37 +0100

    refactor code

--- lib/common/helpers/isExternalLink.ts
+++ lib/common/helpers/isExternalLink.ts
@@ -1,7 +1,6 @@
 export const  isExternalLink = (path: string) : boolean => {
     let operation = false;
     let url;
-
         try {
             url = new URL(path);
             window.open(url,'_blank',   'noopener');

87a62c75fea45d3d9c3470e00e81c07f74617a09
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-01-06 11:46:14 +0100

    center text in Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,6 +4,9 @@ import ContactForm from "./ContactForm";
 const Contact = (): ReactElement => {
     return (
         <div id="Contact" className="bg-pink w-screen h-screen ">
+            <div className="text-center">
+                <h1>Get in Touch</h1>
+            </div>
                 <ContactForm />
         </div>
     );

1dcc898d6f326d5023530ba0710db76ddc6c58d3
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-01-07 15:12:16 +0100

    refactor code in Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,9 +3,10 @@ import ContactForm from "./ContactForm";
 
 const Contact = (): ReactElement => {
     return (
-        <div id="Contact" className="bg-pink w-screen ">
+        <div id="Contact" className="bg-pink w-screen">
             <div className="text-center">
-                <h1>Get in Touch</h1>
+                <h1 className="font-bold text-lg">Get in Touch</h1>
+                <p>I'll get back to you within 24 hours.</p>
             </div>
             <div className="flex h-screen justify-center items-center">
                 <ContactForm />

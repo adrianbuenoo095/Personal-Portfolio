0f0354bff91420f8bd71f7e1bf813994c4f38847
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-01-08 16:45:52 +0100

    reformat code

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { MenuItems} from "@/components/common/MenuItems";
+import { MenuItems } from "@/components/common/MenuItems";
 import { scrollIntoView } from "@/lib/common/helpers/scrollIntoView";
 import Link from "next/link";
 import { ReactElement, useCallback, useState } from "react";
@@ -7,8 +7,7 @@ import { isExternalLink } from "@/lib/common/helpers/isExternalLink";
 import useScrollControl from "@/lib/common/helpers/useScrollControl";
 
 const NavBar = (): ReactElement => {
-
-    const {openMenu, setOpenMenu}= useScrollControl();
+    const { openMenu, setOpenMenu } = useScrollControl();
 
     const handleClick = useCallback(() => {
         setOpenMenu((prevState) => {
@@ -17,21 +16,28 @@ const NavBar = (): ReactElement => {
     }, []);
 
     return (
-        //new: hidden sm:ml-6 sm:block
-        //old: md:hidden fixed top-5 right-0 bg-gray-dark text-white p-4' : 'my-11 hidden md:block'
-
         <>
-            <nav className={ openMenu ? 'md:hidden fixed top-5 right-0 bg-gray-dark text-white p-4' : 'hidden md:block fixed top-5 text-white'}>
-                {MenuItems.map(( {name, path,id} ) => (
+            <nav
+                className={
+                    openMenu
+                        ? "md:hidden fixed top-5 right-0 bg-gray-dark text-white p-4"
+                        : "hidden md:block fixed top-5 text-white"
+                }
+            >
+                {MenuItems.map(({ name, path, id }) => (
                     <div
                         key={name}
-                        className={openMenu ? 'flex flex-col gap-5' : 'p-3 inline-flex text-black font-sans text-lg hover:text-pink' }
+                        className={
+                            openMenu
+                                ? "flex flex-col gap-5"
+                                : "p-3 inline-flex text-black font-sans text-lg hover:text-pink"
+                        }
                     >
                         <Link
                             href={path}
                             onClick={(e) => {
                                 e.preventDefault();
-                                isExternalLink(path)
+                                isExternalLink(path);
                                 scrollIntoView(id);
                             }}
                         >
